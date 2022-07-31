960b03aeafd616f73abca4afec543cbad136f185
demetrx <mcbright7447@gmail.com>
2022-07-31 13:22:20 +0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,75 @@ import { Component } from 'react';
 
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
+import pixabayAPI from '../services/pixabay-api';
 
 export class App extends Component {
   state = {
     query: '',
+    loading: false,
+    page: 1,
+    largeImg: null,
+    items: [],
   };
+
+  componentDidUpdate(_, prevState) {
+    const prevQuery = prevState.query;
+    const nextQuery = this.state.query;
+
+    if (prevQuery !== nextQuery) {
+      this.setState({ page: 1 });
+      return this.fetchItems(true);
+    }
+
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+
+    if (prevPage !== nextPage && nextPage !== 1) {
+      this.fetchItems(false);
+    }
+  }
+
+  fetchItems = isNewQuery => {
+    const { query } = this.state;
+    const page = isNewQuery ? 1 : this.state.page;
+
+    this.setState({ loading: true });
+
+    pixabayAPI
+      .fetchImages(query, page)
+      .then(data => {
+        const items = data.hits.map(({ id, webformatURL, largeImageURL }) => ({ id, webformatURL, largeImageURL }));
+        return this.setState(prevState => ({
+          items: isNewQuery ? items : [...prevState.items, ...items],
+        }))
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleImgClick = largeImg => {
+    this.setState({ largeImg });
+  };
+
+  handleModalClose() {
+    this.setState({ largeImg: null });
+  }
 
   handleSearchChange = newQuery => {
     this.setState({ query: newQuery });
   };
 
   render() {
-    const { query } = this.state;
+    const { query, items, loading, largeImg, } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleSearchChange} />
-        <ImageGallery query={query} />
+        {items.length !== 0 && <ImageGallery query={query} items={items} loading={loading} largeImg={largeImg} onLoadMore={this.handleLoadMore} onImgClick={this.handleImgClick} />}
       </>
     );
   }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,101 +1,49 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Button from 'components/UI/Button/Button';
 import Loader from 'components/UI/Loader/Loader';
 import Modal from 'components/UI/Modal/Modal';
-
-import pixabayAPI from '../../services/pixabay-api';
 import { ImageGalleryStyled } from './ImageGallery.styled';
 
-class ImageGallery extends Component {
-  static propTypes = {
-    query: PropTypes.string,
-  }
+const ImageGallery = ({ query, items, loading, largeImg, onLoadMore, onImgClick }) => {
+  return (
+    <div>
+      <ImageGalleryStyled id="gallery">
+        {items.map(({ id, webformatURL, largeImageURL }) => (
+          <ImageGalleryItem
+            onClick={onImgClick}
+            key={id}
+            small={webformatURL}
+            large={largeImageURL}
+            alt={query}
+          />
+        ))}
+      </ImageGalleryStyled>
 
-  state = { loading: false, page: 1, largeImg: null, items: [] };
+      {loading && <Loader />}
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.query;
-    const nextQuery = this.props.query;
+      {items.length > 0 && !loading && (
+        <Button onClick={onLoadMore} label="Load more" />
+      )}
 
-    if (prevQuery !== nextQuery) {
-      this.setState({ page: 1 });
-      return this.fetchItems(true);
-    }
+      {largeImg && (
+        <Modal img={largeImg} alt={query} onHide={onImgClick} />
+      )}
+    </div>
+  );
+}
 
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-
-    if (prevPage !== nextPage && nextPage !== 1) {
-      this.fetchItems(false);
-    }
-  }
-
-  fetchItems = isNewQuery => {
-    const { query } = this.props;
-    const page = isNewQuery ? 1 : this.state.page;
-
-    this.setState({ loading: true });
-
-    pixabayAPI
-      .fetchImages(query, page)
-      .then(data => {
-        const items = data.hits.map(({ id, webformatURL, largeImageURL }) => ({ id, webformatURL, largeImageURL }));
-        return this.setState(prevState => ({
-          items: isNewQuery ? items : [...prevState.items, ...items],
-        }))
-      })
-      .finally(() => {
-        this.setState({ loading: false });
-      });
-  };
-
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  handleImgClick = largeImg => {
-    this.setState({ largeImg });
-  };
-
-  handleModalClose() {
-    this.setState({ largeImg: null });
-  }
-
-  render() {
-    const { items, loading, largeImg } = this.state;
-    const { query } = this.props;
-
-    if (items.length === 0) return null;
-
-    return (
-      <div>
-        <ImageGalleryStyled id="gallery">
-          {items.map(({ id, webformatURL, largeImageURL }) => (
-            <ImageGalleryItem
-              onClick={this.handleImgClick}
-              key={id}
-              small={webformatURL}
-              large={largeImageURL}
-              alt={query}
-            />
-          ))}
-        </ImageGalleryStyled>
-
-        {loading && <Loader />}
-
-        {items.length > 0 && !loading && (
-          <Button onClick={this.handleLoadMore} label="Load more" />
-        )}
-
-        {largeImg && (
-          <Modal img={largeImg} alt={query} onHide={this.handleImgClick} />
-        )}
-      </div>
-    );
-  }
+ImageGallery.propTypes = {
+  query: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  largeImg: PropTypes.string,
+  onLoadMore: PropTypes.func.isRequired,
+  onImgClick: PropTypes.func.isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+  })),
 }
 
 export default ImageGallery;
